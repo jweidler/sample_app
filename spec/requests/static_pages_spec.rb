f8c0e8b151ceb556169e256d552830f4b65969c8
Jonas Weidler <mail@jonas-weidler.de>
2012-08-01 18:07:31 +0200

    add contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -38,4 +38,16 @@ describe "Static Pages" do
     end
   end
 
+  describe "Contact page" do
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => "Contact")
+    end
+    it "should have the title 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', 
+          :text => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+
 end
